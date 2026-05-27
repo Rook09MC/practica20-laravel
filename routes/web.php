9e5c7416f2b5ce821a739b94523731abf10eed7d
Rook09MC <drackhe00@gmail.com>
2026-05-27 18:06:04 -0400

    Versión estable: auth funcionando y rutas corregidas

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,9 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
-
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 Route::get('/admin', [AdminController::class, 'dashboard'])
     ->middleware(['auth', 'admin'])
     ->name('admin');
